c1529f0c3f1fe93b7252ea5fab0abec59e1fde08
yh-ki <84252233+yh-ki@users.noreply.github.com>
2022-03-03 20:24:41 -0500

    Add files via upload

---
+++ compile.rkt
@@ -0,0 +1,86 @@
+#lang racket
+(provide (all-defined-out))
+(require "ast.rkt" "types.rkt" a86/ast)
+
+;; Expr -> Asm
+(define (compile e)
+  (prog (Label 'entry)
+        (compile-e e)
+        (Ret)))
+
+;; Expr -> Asm
+(define (compile-e e)
+  (match e
+    [(Int i)           (compile-integer i)]
+    [(Bool b)          (compile-boolean b)]
+    [(Prim1 p e)       (compile-prim p e)]
+    [(If e1 e2 e3)     (compile-if e1 e2 e3)]
+    ;; TODO: Handle cond
+    ;; TODO: Handle case
+    ))
+
+
+;; Integer -> Asm
+(define (compile-integer i)
+  (seq (Mov 'rax (value->bits i))))
+
+;; Boolean -> Asm
+(define (compile-boolean b)
+  (seq (Mov 'rax (value->bits b))))
+
+;; Op Expr -> Asm
+(define (compile-prim p e)
+  (seq (compile-e e)
+       (match p
+         ['add1 (Add 'rax (value->bits 1))]
+         ['sub1 (Sub 'rax (value->bits 1))]
+         ;; TODO: Handle abs, -, and not
+         ['zero?
+          (let ((l1 (gensym 'nzero)))
+            (seq (Cmp 'rax 0)
+                 (Mov 'rax val-true)
+                 (Je l1)
+                 (Mov 'rax val-false)
+                 (Label l1)))]
+         ['abs
+            (seq (Cmp 'rax 0)
+                 (Jg 'done)
+                 (Mov 'rbx (value->bits 0))
+                 (Sub 'rbx 'rax)
+                 (Push 'rbx)
+                 (Pop 'rax)
+                 (Label 'done)
+                 (ret))]
+         ['-
+          (seq (Mov 'rbx (value->bits 0))
+               (Sub 'rbx 'rax)
+               (Push 'rbx)
+               (Pop 'rax))]
+         ['not
+          (seq (Cmp 'rax val-false)
+               (Je 'l1)
+               (Move 'rax val-false)
+               (ret)
+               (Label 'l1)
+               (Move 'rax val-true)
+               (ret))])))
+
+;; Expr Expr Expr -> Asm
+(define (compile-if e1 e2 e3)
+  (let ((l1 (gensym 'if))
+        (l2 (gensym 'if)))
+    (seq (compile-e e1)
+         (Cmp 'rax val-false)
+         (Je l1)
+         (compile-e e2)
+         (Jmp l2)
+         (Label l1)
+         (compile-e e3)
+         (Label l2))))
+  
+
+
+
+
+
+
