ecf97eee751b6df66248002b39cd1785c40cca7f
yh-ki <84252233+yh-ki@users.noreply.github.com>
2022-03-04 02:38:25 -0500

    Add files via upload

--- parse.rkt
+++ parse.rkt
@@ -16,7 +16,7 @@
     [(list 'if e1 e2 e3)
      (If (parse e1) (parse e2) (parse e3))]
     ;; TODO: Handle cond
-    [(list 'cond x ... ['else e]) (Cond (parse_cond x) (parse e))]
+    [(list 'cond x ... ["else" e]) (Cond (parse_cond x) (parse e))]
     ;; TODO: Handle case
     ;; TODO: Remove this clause once you've added clauses for
     ;; parsing cond and case; it's here just so running the test suite
