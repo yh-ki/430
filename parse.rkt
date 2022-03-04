136d424f4467f3dbd3593e51668d557fcd655827
yh-ki <84252233+yh-ki@users.noreply.github.com>
2022-03-04 04:13:14 -0500

    Add files via upload

--- parse.rkt
+++ parse.rkt
@@ -9,6 +9,7 @@
     [(? boolean?) (Bool s)]
     [(list 'add1 e)  (Prim1 'add1 (parse e))]
     [(list 'sub1 e)  (Prim1 'sub1 (parse e))]
+    [(list 'zero e) (Prim1 'zero (parse e))]
     ;; TODO: Handle abs, - and not
     [(list 'abs e)  (Prim1 'abs (parse e))]
     [(list '- e)  (Prim1 '- (parse e))]
